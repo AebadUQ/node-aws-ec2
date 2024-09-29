b229ee846007f55fae6b39f56af01e3f4a60aaa0
huzaifacodevative <aebad@codevative.com>
2024-09-30 01:12:36 +0500

    setup

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "hi from server" });
+  res.json({ message: "Hi from Aebad's Server" });
 });
 
 const PORT = 3000;
